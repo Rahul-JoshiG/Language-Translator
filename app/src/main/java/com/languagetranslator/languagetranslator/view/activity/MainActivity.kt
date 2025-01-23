165b403466e0a506740a9f7441888085e34e4049
Rahul Joshi <143573710+Rahul-JoshiG@users.noreply.github.com>
2025-01-23 22:03:33 +0530

    update the navigation with navigation graph

--- app/src/main/java/com/languagetranslator/languagetranslator/view/activity/MainActivity.kt
+++ app/src/main/java/com/languagetranslator/languagetranslator/view/activity/MainActivity.kt
@@ -10,72 +10,79 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.languagetranslator.languagetranslator.R
 import com.languagetranslator.languagetranslator.databinding.ActivityMainBinding
 import com.languagetranslator.languagetranslator.utils.NetworkUtils
-import com.languagetranslator.languagetranslator.view.fragments.ImageFragment
-import com.languagetranslator.languagetranslator.view.fragments.SpeechFragment
-import com.languagetranslator.languagetranslator.view.fragments.TextFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-    private val mTextFragment by lazy { TextFragment() }
-    private val mImageFragment by lazy { ImageFragment() }
-    private val mSpeechFragment by lazy { SpeechFragment() }
+    private lateinit var mNavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "onCreate: create activity")
         enableEdgeToEdge()
+
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        //checking the internet connection
+        applyWindowInsets()
+
+        // Check internet connection
         checkInternetConnection()
 
-        //chang the color of status bar
+        // Change the color of the status bar
         changeStatusBarColor()
 
-        // Apply system bars padding
+        setUpNavigation() // Setup navigation
+
+        // Set default fragment using the navigation graph
+        if (savedInstanceState == null) {
+            mNavController.navigate(R.id.textFragment) // or whichever is the initial fragment in the graph
+        }
+
+        // Set up Bottom Navigation View
+        mBinding.bottomNav.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.text -> {
+                    mNavController.navigate(R.id.textFragment)
+                    true
+                }
+                R.id.image -> {
+                    mNavController.navigate(R.id.imageFragment)
+                    true
+                }
+                R.id.speech -> {
+                    mNavController.navigate(R.id.speechFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun setUpNavigation() {
+        Log.d(TAG, "setUpNavigation: ")
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        mNavController = navHostFragment.navController
+        NavigationUI.setupWithNavController(mBinding.bottomNav, mNavController)
+    }
+
+    private fun applyWindowInsets() {
+        Log.d(TAG, "applyWindowInsets: ")
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        //By default fragment
-        openFragment(mTextFragment)
-
-        //bottom navigation bar item selection
-        mBinding.bottomNav.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-
-                R.id.text -> {
-                    openFragment(mTextFragment)
-                    true
-                }
-
-                R.id.image -> {
-                    openFragment(mImageFragment)
-                    true
-                }
-
-                R.id.speech -> {
-                    openFragment(mSpeechFragment)
-                    true
-                }
-
-                else -> false
-            }
-        }
-
     }
 
-
-    //check the internet is available or not
     private fun checkInternetConnection() {
         Log.d(TAG, "checkInternet: checking internet connection")
         if (NetworkUtils.isInternetAvailable()) {
@@ -85,7 +92,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    //If internet is not available then shows the dialog box
     fun showNoInternetDialog() {
         val dialog = AlertDialog.Builder(this)
             .setTitle("No Internet Connection")
@@ -105,8 +111,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         dialog.show()
     }
 
-
-    //status bar color change
     @Suppress("DEPRECATION")
     private fun changeStatusBarColor() {
         val window = this.window
@@ -115,26 +119,6 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         window.statusBarColor = this.resources.getColor(R.color.color_primary)
         window.decorView.systemUiVisibility =
             window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR.inv()
-
-    }
-
-    //All clicks are here
-    override fun onClick(view: View?) {
-        Log.d(TAG, "onClick: $view clicked")
-        when (view?.id) {
-            R.id.text -> openFragment(mTextFragment)
-            R.id.image -> openFragment(mImageFragment)
-            R.id.speech -> openFragment(mSpeechFragment)
-        }
-    }
-
-    //open the fragment
-    private fun openFragment(fragment: Fragment) {
-        Log.d(TAG, "openFragment: opening fragment $fragment")
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container_view, fragment)
-        transaction.commit()
-        Log.d(TAG, "openFragment: ${fragment::class.java.simpleName}")
     }
 
     companion object {
