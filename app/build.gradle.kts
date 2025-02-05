8e0a8f6b0bea7a3969ed312b45d551d1ea5e6c02
Rahul Joshi <143573710+Rahul-JoshiG@users.noreply.github.com>
2025-02-05 11:22:45 +0530

    update the version of app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.languagetranslator.languagetranslator"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.4.1"
+        versionCode = 2
+        versionName = "2.0.0"
 
 
         buildConfigField("String", "GEMINI_API_KEY", properties.getProperty("GEMINI_API_KEY"))
